8b2922064314678df61325ee8042526a65a9306b
hinh1108 <hinh1108@gmail.com>
2017-02-14 22:18:28 +0700

    firt

---
+++ app/src/main/java/zuize/riddles/MainActivity.java
@@ -0,0 +1,13 @@
+package zuize.riddles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
